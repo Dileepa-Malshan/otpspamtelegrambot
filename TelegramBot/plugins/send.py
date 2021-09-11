82088822609ded48b5f7a69f29c66506833cb6f1
Dileepa_Malshan <84204637+Dileepa-Malshan@users.noreply.github.com>
2021-09-10 23:12:14 -0700

    Add files via upload

---
+++ TelegramBot/plugins/send.py
@@ -0,0 +1,31 @@
+from .. import bot
+from telethon import events
+import requests
+from config import vars
+import time,os
+try:
+    from requests import post,get
+
+
+def send_message(number):
+    base_url = 'https://guru.lk/auth/headstart/ajax.php'
+    params = {
+        'phone':number,'course':'1','sesskey':'','action':'sms_reg'
+            }
+    if len(message) <= vars.max:
+        result = requests.get(base_url, params=params) 
+        result_text = result.text
+        status_code = result.status_code
+    else:
+        result_text = f'Max message length should be {vars.max} letters.'
+        status_code = None
+    return result_text, status_code
+
+@bot.on(events.NewMessage(pattern='/send$', func=lambda e: e.is_private))
+async def send(event):
+    async with bot.conversation(await event.get_chat(), exclusive=False, total_timeout=600) as conv:
+        await conv.send_message("Send me SRILANKA 🇮🇳 number without +94 in 60 seconds.")
+        phone_number = await conv.get_response(timeout=(60))      
+        sms_resp, status_code = send_message(phone_number.text,  message.text)
+        await conv.send_message(f"**Response from API** : `{sms_resp}`.\n**Status code** : `{status_code}`\nThanks for using this bot.")
+
